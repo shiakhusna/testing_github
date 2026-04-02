4980c0d43d306703476889eafd2c7443bbb005de
shaik husna <shaikhusna@gmail.com>
2026-04-02 17:19:42 +0530

    Initial commit

--- python_basics_shaikhusna.py
+++ python_basics_shaikhusna.py
@@ -2,15 +2,13 @@
 name = "Shaik Husna"  #string
 age = 31              #integer
 height = 5.6          #float
-is_stdent = False     #boolean
+is_student = False     #boolean
 print (name, age, height, is_student)
-
 # ___ Task 2: String Formatting ___
 # Using f-string for the required format 
-print (f"Name: {Husna}") | Age: {31} | Height: {5.6} | Student: {is_student}")
-
+print (f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
 # ___Task 3: Arithmetic Operations ___
-age_in_monthss = 31 * 12
-age_in_days = 31 *365
-remainder_by_7 = 31 % 7
-age_squared = 31 ** 2
+age_in_monthss = age * 12
+age_in_days = age *365
+remainder_by_7 = age % 7
+age_squared = age ** 2
