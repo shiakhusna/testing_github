00060860fdd33fb7e77546f73da5544e0443d28d
shaik husna <shaikhusna@gmail.com>
2026-04-02 16:48:03 +0530

    Initial commit

---
+++ python_basics_shaikhusna.py
@@ -0,0 +1,16 @@
+# ___ Task 1: Variables and Data Types ___
+name = "Shaik Husna"  #string
+age = 31              #integer
+height = 5.6          #float
+is_stdent = False     #boolean
+print (name, age, height, is_student)
+
+# ___ Task 2: String Formatting ___
+# Using f-string for the required format 
+print (f"Name: {Husna}") | Age: {31} | Height: {5.6} | Student: {is_student}")
+
+# ___Task 3: Arithmetic Operations ___
+age_in_monthss = 31 * 12
+age_in_days = 31 *365
+remainder_by_7 = 31 % 7
+age_squared = 31 ** 2
